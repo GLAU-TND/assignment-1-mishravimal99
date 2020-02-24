35124bd7412f52826c6611dd15bcf32f231cca8a
Vimal Mishra <vimal.mishra_cs18@gla.ac.in>
2020-02-24 17:03:24 +0530

    completing Person.java

--- src/Person/Person.java
+++ src/Person/Person.java
@@ -1,4 +1,54 @@
-package Person;
+package person;
+
+import java.util.ArrayList;
 
 public class Person {
+    // a field to store the first name
+    // a field to store the last name
+    // a list to store multiple contact numbers (i.e a list of numbers)
+    // a field to store the email address of the person
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<String> contacts;
+    private String email;
+
+    public Person(String firstName,String lastName,ArrayList<String> contacts,String email){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.contacts=contacts;
+        this.email=email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<String> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(ArrayList<String> contacts) {
+        this.contacts = contacts;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
